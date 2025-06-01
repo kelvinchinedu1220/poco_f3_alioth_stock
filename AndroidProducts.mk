8d1ce54e90a7cc3bed1354bbda8f2a1263fd3340
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-06-01 08:44:31 +0100

    Add files via upload

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for Redmi M2012K11AC (alioth)
-
-```
 #
 # Copyright (C) 2025 The Android Open Source Project
 # Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_alioth.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_alioth-user \
+    omni_alioth-userdebug \
+    omni_alioth-eng
