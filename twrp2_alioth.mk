4b2cfedfc9e3db932746e9e6f52bbedd077fc5bd
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-07-03 21:41:40 +0100

    Update and rename omni_alioth.mk to twrp2_alioth.mk

---
+++ twrp2_alioth.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from this product for devices that support only 64-bit apps using:
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Inherit from alioth device
+$(call inherit-product, device/xiaomi/alioth/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := alioth
+PRODUCT_NAME := twrp_alioth
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := F3
+PRODUCT_MANUFACTURER := xiaomi
