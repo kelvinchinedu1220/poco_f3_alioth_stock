32603924909de7c0a526acf17948dd310c421e8f
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-07-03 21:01:02 +0100

    Delete recovery/root/system/bin/pre_rom_flash.sh

--- recovery/root/system/bin/pre_rom_flash.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-echo "I: Running pre-ROM-flash script..." >> /tmp/recovery.log;
-echo "I: Arguments received=\"$@\"" >> /tmp/recovery.log;
-mkdir -p /data/cache/recovery/;
-exit 0;
