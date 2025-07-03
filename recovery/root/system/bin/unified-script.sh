60f5921964232f7ebe8118d58a31ec88a66b64ee
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-07-03 21:05:42 +0100

    Update unified-script.sh

--- recovery/root/system/bin/unified-script.sh
+++ recovery/root/system/bin/unified-script.sh
@@ -3,12 +3,12 @@
 
 load_CN()
 {
-    resetprop "ro.product.model" "K40S"
-    resetprop "ro.product.odm.model" "K40S"
-    resetprop "ro.product.system.model" "K40S"
-    resetprop "ro.product.vendor.model" "K40S"
-    resetprop "ro.product.system_ext.model" "K40S"
-    resetprop "ro.product.product.model" "K40S"
+    resetprop "ro.product.model" "K40"
+    resetprop "ro.product.odm.model" "K40"
+    resetprop "ro.product.system.model" "K40"
+    resetprop "ro.product.vendor.model" "K40"
+    resetprop "ro.product.system_ext.model" "K40"
+    resetprop "ro.product.product.model" "K40"
     resetprop "ro.product.brand" "Xiaomi"
     resetprop "ro.product.odm.brand" "Xiaomi"
     resetprop "ro.product.system.brand" "Xiaomi"
@@ -19,12 +19,12 @@ load_CN()
 
 load_global()
 {
-    resetprop "ro.product.model" "F4"
-    resetprop "ro.product.odm.model" "F4"
-    resetprop "ro.product.system.model" "F4"
-    resetprop "ro.product.vendor.model" "F4"
-    resetprop "ro.product.system_ext.model" "F4"
-    resetprop "ro.product.product.model" "F4"
+    resetprop "ro.product.model" "F3"
+    resetprop "ro.product.odm.model" "F3"
+    resetprop "ro.product.system.model" "F3"
+    resetprop "ro.product.vendor.model" "F3"
+    resetprop "ro.product.system_ext.model" "F3"
+    resetprop "ro.product.product.model" "F3"
     resetprop "ro.product.brand" "POCO"
     resetprop "ro.product.odm.brand" "POCO"
     resetprop "ro.product.system.brand" "POCO"
